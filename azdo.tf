a13a11fa916db896bd6ce52906771ef13edcdac6
Alexander <alex.seppenen@gmail.com>
2023-01-13 18:46:48 +0200

    output updates

--- azdo.tf
+++ azdo.tf
@@ -95,12 +95,7 @@ resource "azuredevops_resource_authorization" "kv_auth" {
   authorized  = true
 }
 
-data "azuredevops_git_repository" "single-repo" {
-  project_id = azuredevops_project.this.id
-  name       = local.ado_repository_name
-}
-
 output "repository_url" {
-  value = data.azuredevops_git_repository.single-repo.remote_url
+  value = azuredevops_git_repository.repo.remote_url
   description = "This is the URL that you would use to clone the repository. It is the same as the repository URL in the Azure DevOps UI."
 }
