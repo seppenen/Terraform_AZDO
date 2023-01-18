5c79e091591ce6a81cf45e3aa92ab1898c99954b
seppeale <alex.seppenen@gmail.com>
2023-01-18 14:22:14 +0200

    K8s cluster creation

--- environment.tf
+++ environment.tf
@@ -1,3 +1,8 @@
+resource "azurerm_resource_group" "this" {
+  name     = local.az_resource_group_name
+  location = var.az_location
+}
+
 resource "azurerm_container_registry" "acr" {
   name                = local.az_container_name
   resource_group_name = azurerm_resource_group.this.name
--- variables.tf
+++ variables.tf
@@ -65,6 +65,6 @@ locals {
   host                          = azurerm_kubernetes_cluster.this.kube_config.0.host
   client_key                    = base64decode(azurerm_kubernetes_cluster.this.kube_config.0.client_key)
   client_certificate            = base64decode(azurerm_kubernetes_cluster.this.kube_config.0.client_certificate)
+  cluster_ca_certificate        = base64decode(azurerm_kubernetes_cluster.this.kube_config.0.cluster_ca_certificate)
   kube_config                   = azurerm_kubernetes_cluster.this.kube_config_raw
-  cluster_ca_certificate        = base64decode(azurerm_kubernetes_cluster.this.kube_config.0.client_certificate)
 }
